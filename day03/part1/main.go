a6a78f6c5977d62f5add0aa895915451da1ee8be
Gergely Brautigam <gergely@arangodb.com>
2019-12-04 17:45:32 +0100

    Using a map.. too lazy to fix part2 you get the idea. :)

--- day03/part1/main.go
+++ day03/part1/main.go
@@ -16,47 +16,43 @@ var dir = map[string][]int{
 	"U": {1, 0},
 }
 
+type point struct {
+	x, y int
+}
+
 func main() {
 	filename := os.Args[1]
 	content, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(content), "\n")
 	w1 := lines[0]
 	w2 := lines[1]
-	// min := math.MaxInt64
 	w1Split := strings.Split(w1, ",")
-	grid := make([][]int, 100000)
-	for g := 0; g < 100000; g++ {
-		grid[g] = make([]int, 100000)
-	}
-	startX := 50000
-	startY := 50000
-	currX := startX
-	currY := startY
-	// Fill in the first wire.
+	grid := make(map[point]bool)
+	start := point{0, 0}
+	curr := start
+	grid[start] = true
 	for _, s := range w1Split {
 		d, n := s[0], s[1:]
 		distance, _ := strconv.Atoi(n)
 		heading := dir[string(d)]
 		for i := 0; i < distance; i++ {
-			currY, currX = currY+heading[0], currX+heading[1]
-			grid[currY][currX] = 1
+			curr.y, curr.x = curr.y+heading[0], curr.x+heading[1]
+			grid[curr] = true
 		}
 	}
 
-	// Fill in the second wire and record intersections and their distances.
 	min := math.MaxInt64
 	w2Split := strings.Split(w2, ",")
-	currX = startX
-	currY = startY
+	curr = start
 	for _, s := range w2Split {
 		d, n := s[0], s[1:]
 		distance, _ := strconv.Atoi(n)
 		heading := dir[string(d)]
 		for i := 0; i < distance; i++ {
-			currY, currX = currY+heading[0], currX+heading[1]
-			if grid[currY][currX] == 1 {
-				m := abs(startX-currX) + abs(startY-currY)
-				if m < min && m > 0 {
+			curr.y, curr.x = curr.y+heading[0], curr.x+heading[1]
+			if grid[curr] {
+				m := abs(start.x-curr.x) + abs(start.y-curr.y)
+				if m < min && m > 1 {
 					min = m
 				}
 			}
