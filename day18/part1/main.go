dc9b1e34af707b2c279f18fb2083f4b8901dadf3
Gergely Brautigam <gergely@arangodb.com>
2020-11-27 07:22:41 +0100

    not yet

--- day18/part1/main.go
+++ day18/part1/main.go
@@ -10,6 +10,12 @@ var content = `#########
 #b.A.@.a#
 #########`
 
+var content2 = `########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################`
+
 type point struct {
 	x, y int
 }
@@ -21,12 +27,18 @@ var directions = []point{
 	{y: 0, x: 1},
 }
 
-func walk(current point, maze []string, keys map[string]struct{}) []point {
+func walk(current point, maze []string, keys map[byte]struct{}) []point {
 	moves := make([]point, 0)
 	for _, d := range directions {
 		next := point{x: current.x + d.x, y: current.y + d.y}
 		if maze[next.y][next.x] != '#' {
-			moves = append(moves, next)
+			if unicode.IsUpper(rune(maze[next.y][next.x])) {
+				if _, ok := keys[byte(unicode.ToLower(rune(maze[next.y][next.x])))]; ok {
+					moves = append(moves, next)
+				}
+			} else {
+				moves = append(moves, next)
+			}
 		}
 	}
 	return moves
@@ -66,10 +78,11 @@ func main() {
 
 	// End goal is to visit all locations for now. But in fact, if we don't find any new keys, I guess.
 	// We first, have to collect all keys and then keep track if we have them all.
-	keys := make(map[string]struct{})
+	keys := make(map[byte]struct{})
 	seen := make(map[point]struct{})
 	queue := []point{start}
 	var current point
+	var steps int
 
 	fmt.Println()
 	for len(queue) > 0 {
@@ -77,12 +90,17 @@ func main() {
 		fmt.Print(string(maze[current.y][current.x]))
 		if _, ok := seen[current]; !ok {
 			seen[current] = struct{}{}
+			if unicode.IsLower(rune(maze[current.y][current.x])) {
+				keys[maze[current.y][current.x]] = struct{}{}
+			}
 			for _, next := range walk(current, maze, keys) {
 				if _, ok := seen[next]; !ok {
 					queue = append(queue, next)
+					steps++
 				}
 			}
 		}
 	}
 	fmt.Println()
+	fmt.Println("Number of steps: ", steps)
 }
